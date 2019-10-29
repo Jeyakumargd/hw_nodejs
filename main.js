d2f3be489749cd056e44f852553e50e5d0e7405f
Jeyakumargd <56903479+Jeyakumargd@users.noreply.github.com>
2019-10-29 12:37:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-   response.end('Node JS Application is UP & RUNNING \n');
+   response.end('NodeJS Application is UP & RUNNING \n');
 }).listen(8081);
 
 // Console will print the message
